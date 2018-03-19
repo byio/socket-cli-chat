9acb5a5ee1da6d6e7574e91e8e1b372868a1ef45
byio <ben@benyang.io>
2018-03-19 17:10:30 +0800

    set username, and emit a message from client to server that a specific user has joined the chat

--- client.js
+++ client.js
@@ -2,10 +2,22 @@ const readline = require('readline');
 const socketio = require('socket.io-client');
 
 // Init Variables
-const nickname;
+let nickname;
 
 // Setup Client Socket to Make Connection
 const socket = socketio.connect('localhost', {port: 2000});
 
 // Create Readline Interface
 const rl = readline.createInterface(process.stdin, process.stdout);
+
+// Get Username
+rl.question('Please enter a username: ', (username) => {
+  nickname = username;
+  const msg = `${nickname} has joined the chat.`;
+  // Emit Feedback to Server
+  socket.emit('send', {
+    type: 'notice',
+    message: msg
+  });
+  rl.prompt(true);
+});
